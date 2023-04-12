ec23a70d39b0afa48b38e418f9121229646b35e6
joseph shodunke <josephshodunke4@gmail.com>
2023-04-12 20:07:45 +0100

    Delete api2/api directory

--- api2/api/models.py
+++
@@ -1,7 +0,0 @@
-from djongo import models
-
-# Create your models here.
-
-class Start(models.Model):
-    entry = models.CharField(max_length=255)
-    is_completed = models.BooleanField(default=False)
